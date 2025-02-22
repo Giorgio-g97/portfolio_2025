3fa446c61bf80409d6674b1cca00226fa6869dc7
Giorgio-g97 <giorgio.g97@gmail.com>
2025-02-22 10:35:45 +0100

    Aggiungi supporto per il menu mobile nella Navbar e aggiorna le dipendenze

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,100 @@
+"use client";
 import type React from "react";
 import Link from "next/link";
-import {ThemeToggle} from "./ThemeToggle";
+import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MdMenu } from "react-icons/md";
+import { useState } from "react";
 
 export default function Header() {
+  const navItems = [
+    {
+      label: "Competenze",
+      href: "#skills",
+    },
+    {
+      label: "Progetti",
+      href: "#projects",
+    },
+    {
+      label: "Contatti",
+      href: "#contact",
+    },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-background/80 dark:bg-background-dark/80 backdrop-filter backdrop-blur-sm">
-      <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <Link
-          href="/"
-          className="text-xl font-bold text-foreground dark:text-foreground-dark"
-        >
-          G
-        </Link>
-        <div className="flex items-center space-x-6">
+    <>
+      <header className="fixed top-0 left-0 right-0 z-50 bg-background/80 dark:bg-background-dark/80 backdrop-filter backdrop-blur-sm">
+        <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
           <Link
-            href="#skills"
-            className="text-foreground/80 dark:text-foreground-dark/80 hover:text-primary dark:hover:text-primary-dark transition-colors"
+            href="/"
+            className="text-xl font-bold text-foreground dark:text-foreground-dark"
           >
-            Competenze
+            G
           </Link>
-          <Link
-            href="#projects"
-            className="text-foreground/80 dark:text-foreground-dark/80 hover:text-primary dark:hover:text-primary-dark transition-colors"
-          >
-            Progetti
-          </Link>
-          <Link
-            href="#contact"
-            className="text-foreground/80 dark:text-foreground-dark/80 hover:text-primary dark:hover:text-primary-dark transition-colors"
-          >
-            Contatti
-          </Link>
-          <ThemeToggle />
-        </div>
-      </nav>
-    </header>
+          {/* Desktop Navbar */}
+          <div className="hidden md:flex items-center space-x-6">
+            {navItems.map((n, i) => (
+              <Link
+                key={i}
+                href={n.href}
+                className="text-foreground/80 dark:text-foreground-dark/80 hover:text-primary dark:hover:text-primary-dark transition-colors"
+              >
+                {n.label}
+              </Link>
+            ))}
+            <ThemeToggle />
+          </div>
+
+          {/* Mobile Navbar */}
+          <div className="md:hidden flex items-center space-x-4">
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="text-foreground dark:text-foreground-dark"
+                >
+                  <MdMenu />
+                </Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle className="text-right text-primary dark:text-primary-dark text-4xl mt-6">
+                    Menu
+                  </SheetTitle>
+                </SheetHeader>
+                <div className="flex flex-col items-end space-y-10 mt-6">
+                  {navItems.map((n, i) => (
+                    <SheetTrigger asChild>
+                      <Link
+                        key={i}
+                        href={n.href}
+                        className="text-3xl text-foreground/80 dark:text-foreground-dark/80 hover:text-primary dark:hover:text-primary-dark transition-colors"
+                      >
+                        {n.label}
+                      </Link>
+                    </SheetTrigger>
+                  ))}
+                </div>
+              </SheetContent>
+            </Sheet>
+            <ThemeToggle />
+          </div>
+        </nav>
+      </header>
+    </>
   );
 }
