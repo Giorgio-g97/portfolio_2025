5a62d8b42792585a4c9f76951f24230d8541c37c
Giorgio-g97 <giorgio.g97@gmail.com>
2025-02-24 21:34:18 +0100

    Add Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
           <div className="absolute inset-0 bg-gradient-to-br from-primary/20 to-secondary/20 rounded-full filter blur-3xl"></div>
           <div className="relative">
             <Image
-              src="https://images.unsplash.com/photo-1545184180-25d471fe75eb?q=80&w=1861&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="/hero.jpg"
               alt="Giorgio"
               width={400}
               height={400}
